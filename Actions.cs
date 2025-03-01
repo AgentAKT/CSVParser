07795580452c1ab4ca628c8b0d1b94074f7f2cd4
Aleksandr Popov <agentakt@yandex.ru>
2025-03-01 18:46:51 +0300

    Redesign UI

---
+++ Actions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Linq;
+
+
+namespace CSVParser
+{
+    internal class Actions : System.Windows.Window
+    {
+        
+        
+    }
+}
